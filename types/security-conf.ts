0dbc8160d1d40b618fa9902d9b259c8593b28f5b
Chirag Modh <91516739+22chiragmodh@users.noreply.github.com>
2024-08-30 22:04:33 +0530

    fix merge conflict

--- types/security-conf.ts
+++ types/security-conf.ts
@@ -1,11 +1,21 @@
 import mongoose, { PopulatedDoc } from "mongoose";
 
+export enum SecurityConfigType {
+  AUTH_TOKENS = "AUTH_TOKENS",
+  USER_DATA = "USER_DATA",
+  SUCCESS_FLOW = "SUCCESS_FLOW",
+  BROKEN_OBJECT_LEVEL_AUTHORIZATION = "BROKEN_OBJECT_LEVEL_AUTHORIZATION",
+  BROKEN_AUTHENTICATION = "BROKEN_AUTHENTICATION",
+  BROKEN_OBJECT_PROPERTY_LEVEL_AUTHORIZATION = "BROKEN_OBJECT_PROPERTY_LEVEL_AUTHORIZATION",
+  BROKEN_FUNCTION_LEVEL_AUTHORIZATION = "BROKEN_FUNCTION_LEVEL_AUTHORIZATION",
+}
 
 export type SecurityConfiguration = {
-    apiId: PopulatedDoc<mongoose.Types.ObjectId>; // Reference to API
-    configType: string; // This could be an ENUM or a string if you have various types.
-    isEnabled: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-    rules?: Record<string, any>; // Additional metadata if needed
-}
+  apiId: PopulatedDoc<mongoose.Types.ObjectId>; // Reference to API
+  app: PopulatedDoc<mongoose.Types.ObjectId>; // Reference to Application
+  configType: SecurityConfigType; // This could be an ENUM or a string if you have various types.
+  isEnabled: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  rules?: Record<string, any>; // Additional metadata if needed
+};
